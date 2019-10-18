ed611193c5410fbc95eaca105a256244cdcdb9a2
bosua3393 <bosua3393@gmail.com>
2019-10-17 20:56:29 -0400

    add get_state

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import chess
 import agent
 
-board = chess.Board()
-# ai = agent.Agent()
 
+def get_state(board_str):
+    pieces_dict = {'.': 0, 'r': 1, 'n': 2, 'b': 3, 'q': 4, 'k': 5, 'p': 6,
+                           'R': 7, 'N': 8, 'B': 9, 'Q': 10, 'K': 11, 'P': 12}
+    state = []
+    for letter in board_str:
+        if ' ' not in letter and '\n' not in letter:
+            state.append(pieces_dict[letter])
+    print(state)
+    return state
+
+
+board = chess.Board()
 print(board)
+ai = agent.Agent(64)
+
+a = get_state(board.__str__())
+print(len(a))
